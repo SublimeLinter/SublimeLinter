787506fb8e8980a2f472f3a49f55efb543fce940
Ryan Hileman <lunixbochs@gmail.com>
2013-01-08 03:45:00 -0800

    add C++ linter via clang++

--- languages/c.py
+++ languages/c.py
@@ -3,6 +3,18 @@ import os
 from lint.linter import Linter
 from lint.util import find
 
+def find_includes(filename):
+    includes = []
+    if filename:
+        parent = os.path.dirname(filename)
+        includes.append('-I' + parent)
+        inc = find(parent, 'include')
+        if inc:
+            includes.append('-I' + inc)
+
+    return includes
+
+
 class C(Linter):
     language = 'c'
     cmd = ('clang', '-xc', '-fsyntax-only', '-std=c99', '-Werror',
@@ -14,13 +26,10 @@ class C(Linter):
     )
 
     def communicate(self, cmd, code):
-        includes = []
-        if self.filename:
-            parent = os.path.dirname(self.filename)
-            includes.append('-I' + parent)
-            inc = find(parent, 'include')
-            if inc:
-                includes.append('-I' + inc)
-
-        cmd += ('-',) + tuple(includes)
+        cmd += ('-',) + tuple(find_includes(self.filename))
         return super(C, self).communicate(cmd, code)
+
+class CPP(C):
+    language = 'c++'
+    cmd = ('clang++', '-xc++', '-fsyntax-only', '-Werror',
+        '-pedantic')

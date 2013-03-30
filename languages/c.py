94917ea96ec510aa0d38670bdb8063546cb2e895
Ryan Hileman <lunixbochs@gmail.com>
2013-03-30 01:51:03 -0700

    restructure C linter to support plugin settings

--- languages/c.py
+++ languages/c.py
@@ -14,22 +14,35 @@ def find_includes(filename):
 
     return includes
 
-
 class C(Linter):
     language = 'c'
-    cmd = ('clang', '-xc', '-fsyntax-only', '-std=c99', '-Werror',
-        '-pedantic')
+    cmd = ('clang',)
+    args = ('-xc', '-fsyntax-only', '-std=c99', '-Werror', '-pedantic')
     regex = (
         r'^<stdin>:(?P<line>\d+):(?P<col>\d+):'
         r'(?:(?P<ranges>[{}0-9:\-]+):)?\s+'
         r'(?P<error>.+)'
     )
+    defaults = {
+        'cmd': cmd,
+        'args': args,
+        'include': [],
+    }
 
     def communicate(self, cmd, code):
+        cmd = tuple(self.settings.get('cmd'),) or self.cmd
+        cmd += tuple(self.settings.get('args', []))
+        for include in self.settings.get('include', []):
+            cmd += ('-I{}'.format(include),)
         cmd += ('-',) + tuple(find_includes(self.filename))
         return super().communicate(cmd, code)
 
 class CPP(C):
     language = 'c++'
-    cmd = ('clang++', '-xc++', '-fsyntax-only', '-std=c++11', '-Werror',
-        '-pedantic')
+    cmd = ('clang++',)
+    args = ('-xc++', '-fsyntax-only', '-std=c++11', '-Werror', '-pedantic')
+    defaults = {
+        'cmd': cmd,
+        'args': args,
+        'include': [],
+    }

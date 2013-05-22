edc6cd44884fa04d5471100143388904e957d4ec
Ryan Hileman <lunixbochs@gmail.com>
2013-05-21 22:06:08 -0700

    add Puppet linter

--- languages/extras.py
+++ languages/extras.py
@@ -73,6 +73,14 @@ class PHP(Linter):
 
 		return match, row, col, error, near
 
+class Puppet(Linter):
+	language = 'puppet'
+	cmd = ('puppet', 'parser', 'validate', '--color=false')
+	regex = r'^([^:]+:){2}\s*(?P<error>(Syntax error at|Could not match) \'?(?P<near>[^ ]*?)\'?.*) at [^:]*:(?P<line>\d+)$'
+
+	def run(self, cmd, code):
+		return self.tmpfile(cmd, code, suffix='.puppet')
+
 class Ruby(Linter):
 	language = 'ruby'
 	cmd = ('ruby', '-wc')

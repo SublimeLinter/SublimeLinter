238c05070301a9fa0808d2cb44d219abe91a96ec
Aparajita Fishman <aparajita@aparajita.com>
2013-11-03 08:39:20 -0500

    Restore everything

---
+++ lint/__init__.py
@@ -0,0 +1,25 @@
+#
+# lint.__init__
+# Part of SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Ryan Hileman and Aparajita Fishman
+#
+# Project: https://github.com/SublimeLinter/SublimeLinter3
+# License: MIT
+#
+
+from .linter import Linter
+from . import (
+    highlight,
+    linter,
+    persist,
+    util,
+)
+
+__all__ = [
+    'highlight',
+    'Linter',
+    'linter',
+    'persist',
+    'util',
+]

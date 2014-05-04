a72adb96e72921ca35296ae3994dd9d9091261b7
Sonny Vidfamn <sonny.vidfamn@gmail.com>
2014-05-05 01:06:39 +0200

    Fix for behaviour problem in "background mode" with errors not dissapearing after editing the file. Now errors on view are cleared if file is edited.

--- sublimelinter.py
+++ sublimelinter.py
@@ -251,6 +251,7 @@ class SublimeLinter(sublime_plugin.EventListener):
             syntax_changed = False
 
         if syntax_changed or persist.settings.get('lint_mode', 'background') == 'background':
+            self.clear(view)
             self.hit(view)
         else:
             self.clear(view)

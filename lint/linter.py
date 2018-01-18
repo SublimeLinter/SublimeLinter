9a42149a0c2c44fc4c24dfe6d844f4eb9947ac91
herr kaste <herr.kaste@gmail.com>
2018-01-18 10:14:23 +0100

    Print available variables distinct until changed

--- lint/linter.py
+++ lint/linter.py
@@ -485,8 +485,15 @@ class Linter(metaclass=LinterMeta):
         if project_folder:
             variables['folder'] = project_folder
 
-        persist.debug('Available variables: {}'.format(variables))
+        if persist.debug_mode():
+            import pprint
+            self._debug_print_available_variables(pprint.pformat(dict(variables), indent=2))
         return recursive_replace(variables, settings)
+
+    @staticmethod
+    @lru_cache(maxsize=1)
+    def _debug_print_available_variables(variables):
+        persist.debug('Available variables: {}'.format(variables))
 
     @staticmethod
     def _guess_project_path(window, filename):

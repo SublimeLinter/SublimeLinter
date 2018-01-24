6382309151108ca459ee135a8524b2c3742f60c7
herr kaste <herr.kaste@gmail.com>
2018-01-24 10:40:21 +0100

    Cleanup `create_path_dict`

--- panel_view.py
+++ panel_view.py
@@ -160,32 +160,24 @@ def get_filenames(window, bids):
 
 
 def create_path_dict(window, bids):
-    base_dir = ""
     file_names_by_bid = get_filenames(window, bids)
 
-    if len(file_names_by_bid) == 1:
-        for bid, path in file_names_by_bid.items():
-            base_dir, file_name = os.path.split(path)
-            rel_paths = {bid: file_name}
-    else:
-        base_dir = get_common_parent([
-            path
-            for path in file_names_by_bid.values()
-            if not path.startswith('<untitled')
-        ])
-        if not base_dir:
-            rel_paths = file_names_by_bid
-        else:
-            rel_paths = {
-                bid: (
-                    abs_path
-                    if abs_path.startswith('<untitled')
-                    else os.path.relpath(abs_path, base_dir)
-                )
-                for bid, abs_path in file_names_by_bid.items()
-            }
+    base_dir = get_common_parent([
+        path
+        for path in file_names_by_bid.values()
+        if not path.startswith('<untitled')
+    ])
 
-    return rel_paths, base_dir or ""
+    rel_paths = {
+        bid: (
+            abs_path
+            if abs_path.startswith('<untitled')
+            else os.path.relpath(abs_path, base_dir)
+        )
+        for bid, abs_path in file_names_by_bid.items()
+    }
+
+    return rel_paths, base_dir
 
 
 def create_panel(window):

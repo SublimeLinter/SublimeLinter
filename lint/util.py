SublimeLinter/SublimeLinter

commit a3db729367c0ceb423a3f617ab3fa8ab4b739409

fix #864 use random temp files (thanks @kaste)

lint/util.py
@@ -383,15 +383,12 @@ def tmpfile(cmd, code, filename, suffix='', output_stream=STREAM_STDOUT, env=Non
     if suffix:
         filename = os.path.splitext(filename)[0] + suffix
 
-    path = os.path.join(tempdir, filename)
+    file = tempfile.NamedTemporaryFile(delete=False)
+    path = file.name
 
     try:
-        with open(path, mode='wb') as f:
-            if isinstance(code, str):
-                code = code.encode('utf-8')
-
-            f.write(code)
-            f.flush()
+        file.write(bytes(code, 'UTF-8'))
+        file.close()
 
         cmd = list(cmd)
 

commit a083117723e878ff20605215ac46850f2f87d4ca

Fix #911 #912 where a file extension is required for some linters (#913)
regression introduced in a3db729

lint/util.py
@@ -328,15 +328,7 @@ def tmpfile(cmd, code, filename, suffix='', output_stream=STREAM_STDOUT, env=Non
     The result is a string combination of stdout and stderr.
     If env is None, the result of create_environment is used.
     """
-    if not filename:
-        filename = "untitled"
-    else:
-        filename = os.path.basename(filename)
-
-    if suffix:
-        filename = os.path.splitext(filename)[0] + suffix
-
-    file = tempfile.NamedTemporaryFile(delete=False)
+    file = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
     path = file.name
 
     try:
d3ac90c94f0bfe6d5d78be3aae44faa4ed3bb049
herr kaste <herr.kaste@gmail.com>
2018-02-01 11:54:15 +0100

    For compatibility export a pseudo `highlight` from `lint`

--- lint/__init__.py
+++ lint/__init__.py
@@ -7,8 +7,16 @@ from . import (
     util,
 )
 
+from .const import WARNING, ERROR
+
 from .linter import Linter
 from .base_linter.python_linter import PythonLinter
 from .base_linter.ruby_linter import RubyLinter
 from .base_linter.node_linter import NodeLinter
 from .base_linter.composer_linter import ComposerLinter
+
+
+# For compatibility with SL3, export a pseudo highlight class
+class highlight:
+    WARNING=WARNING
+    ERROR=ERROR

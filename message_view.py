93b268907c1d8716ebe585c44d9fc0c832f2935f
Koen Lageveen <koen.lageveen@me.com>
2018-02-06 11:05:09 +0100

    don't list the panel

--- message_view.py
+++ message_view.py
@@ -6,7 +6,7 @@ PANEL_NAME = "SublimeLinter Messages"
 
 class SublimeLinterDisplayPanelCommand(sublime_plugin.TextCommand):
     def run(self, edit, msg=""):
-        panel_view = sublime.active_window().create_output_panel(PANEL_NAME)
+        panel_view = sublime.active_window().create_output_panel(PANEL_NAME, True)
         panel_view.set_read_only(False)
         panel_view.erase(edit, sublime.Region(0, panel_view.size()))
         panel_view.insert(edit, 0, msg)

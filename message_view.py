c3e0b3445079bb8114cb12cdf73b49065b7b84ba
Koen Lageveen <koen.lageveen@me.com>
2018-02-07 20:14:35 +0100

    no need to set readonly to False first

--- message_view.py
+++ message_view.py
@@ -7,7 +7,6 @@ PANEL_NAME = "SublimeLinter Messages"
 class SublimeLinterDisplayPanelCommand(sublime_plugin.WindowCommand):
     def run(self, msg=""):
         panel_view = sublime.active_window().create_output_panel(PANEL_NAME, True)
-        panel_view.set_read_only(False)
         panel_view.run_command('append', {'characters': msg})
         panel_view.set_read_only(True)
         panel_view.show(0)

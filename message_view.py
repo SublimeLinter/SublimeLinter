3b2ae43b1af8384cd7988f15d6470ef9b23aab16
Koen Lageveen <koen.lageveen@me.com>
2018-02-08 09:11:59 +0100

    self.window

--- message_view.py
+++ message_view.py
@@ -5,13 +5,13 @@ PANEL_NAME = "SublimeLinter Messages"
 
 class SublimeLinterDisplayPanelCommand(sublime_plugin.WindowCommand):
     def run(self, msg=""):
-        panel_view = sublime.active_window().create_output_panel(PANEL_NAME, True)
+        panel_view = self.window.create_output_panel(PANEL_NAME, True)
         panel_view.run_command('append', {'characters': msg})
         panel_view.set_read_only(True)
         panel_view.show(0)
-        sublime.active_window().run_command("show_panel", {"panel": "output.{}".format(PANEL_NAME)})
+        self.window.run_command("show_panel", {"panel": "output.{}".format(PANEL_NAME)})
 
 
 class SublimeLinterRemovePanelCommand(sublime_plugin.WindowCommand):
     def run(self):
-        sublime.active_window().destroy_output_panel(PANEL_NAME)
+        self.window.destroy_output_panel(PANEL_NAME)

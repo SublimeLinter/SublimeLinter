ccdade03306784e1d3a9323d0d59ab46e4b20f22
Koen Lageveen <koen.lageveen@me.com>
2018-02-09 17:03:25 +0100

    fix #972 so that's what set_read_only(False) does

--- message_view.py
+++ message_view.py
@@ -6,6 +6,7 @@ PANEL_NAME = "SublimeLinter Messages"
 class SublimeLinterDisplayPanelCommand(sublime_plugin.WindowCommand):
     def run(self, msg=""):
         panel_view = self.window.create_output_panel(PANEL_NAME, True)
+        panel_view.set_read_only(False)
         panel_view.run_command('append', {'characters': msg})
         panel_view.set_read_only(True)
         panel_view.show(0)

1f5cb0262dabde9c1449eb7b5160a0659a25cb17
herr kaste <herr.kaste@gmail.com>
2018-03-12 13:39:57 +0100

    Fast toggle highlighted regions

--- highlight_view.py
+++ highlight_view.py
@@ -49,7 +49,8 @@ def get_regions_keys(view):
 
 State = {
     'active_view': None,
-    'idle_views': set()
+    'idle_views': set(),
+    'quiet_views': set()
 }
 
 
@@ -103,7 +104,8 @@ def on_lint_result(buffer_id, linter_name, **kwargs):
             highlight_regions,
             gutter_regions,
             protected_regions,
-            idle=(view.id() in State['idle_views'])
+            idle=(view.id() in State['idle_views']),
+            quiet=(view.id() in State['quiet_views'])
         )
 
 
@@ -189,6 +191,37 @@ def toggle_demoted_regions(view, show):
 
             regions = view.get_regions(key)
             view.add_regions(key, regions, scope=scope, flags=flags)
+
+
+class SublimeLinterToggleHighlights(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view()
+        vid = view.id()
+        hidden = vid in State['quiet_views']
+        if hidden:
+            State['quiet_views'].discard(vid)
+        else:
+            State['quiet_views'].add(vid)
+
+        toggle_all_regions(view, show=hidden)
+
+
+HIDDEN_SCOPE = ''
+
+
+def toggle_all_regions(view, show):
+    region_keys = get_regions_keys(view)
+    for key in region_keys:
+        if '.Highlights.' not in key:
+            continue
+
+        _namespace, scope, flags = key.split('|')
+        flags = int(flags)
+        if not show:
+            scope = HIDDEN_SCOPE
+
+        regions = view.get_regions(key)
+        view.add_regions(key, regions, scope=scope, flags=flags)
 
 
 def invalidate_regions_under_cursor(view):
@@ -371,7 +404,7 @@ def undraw(view):
 
 
 def draw(view, linter_name, highlight_regions, gutter_regions,
-         protected_regions, idle):
+         protected_regions, idle, quiet):
     """
     Draw code and gutter marks in the given view.
 
@@ -402,7 +435,9 @@ def draw(view, linter_name, highlight_regions, gutter_regions,
 
     # otherwise update (or create) regions
     for region_id, (scope, flags, regions) in highlight_regions.items():
-        if not idle and DEMOTE_WHILE_BUSY_MARKER in region_id:
+        if quiet:
+            scope = HIDDEN_SCOPE
+        elif not idle and DEMOTE_WHILE_BUSY_MARKER in region_id:
             scope = get_demote_scope()
         view.add_regions(region_id, regions, scope=scope, flags=flags)
 

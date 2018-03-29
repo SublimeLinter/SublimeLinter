03ecf78f39f1cc1ae4a33e0e79cad00b433c21dc
herr kaste <herr.kaste@gmail.com>
2018-03-29 21:19:53 +0200

    Show warning/error count right beside linter name in status bar

--- active_linters_view.py
+++ active_linters_view.py
@@ -4,12 +4,15 @@ import sublime
 import sublime_plugin
 
 from .lint import events, persist
+from .lint.const import WARNING, ERROR
 
 
 STATUS_ACTIVE_KEY = 'sublime_linter_status_active'
 
 State = {
-    'active_linters_per_bid': defaultdict(set),
+    'assigned_linters_per_bid': defaultdict(set),
+    'failed_linters_per_bid': defaultdict(set),
+    'problems_per_bid': defaultdict(dict),
     'needs_redraw': set()
 }
 
@@ -23,50 +26,67 @@ def plugin_unloaded():
 
 
 @events.on(events.LINT_RESULT)
-def redraw_bid(buffer_id, **kwargs):
-    if buffer_id not in State['needs_redraw']:
-        return
+def redraw_bid(buffer_id, linter_name, errors, **kwargs):
+    problems = State['problems_per_bid'][buffer_id]
+    if linter_name in State['failed_linters_per_bid'][buffer_id]:
+        problems[linter_name] = '(erred)'
+    elif linter_name in State['assigned_linters_per_bid'][buffer_id]:
+        we_count = count_problems(errors)
+        if we_count == (0, 0):
+            problems[linter_name] = '(ok)'
+        else:
+            problems[linter_name] = '({}|{})'.format(*we_count)
+    else:
+        problems.pop(linter_name, None)
 
-    State['needs_redraw'].discard(buffer_id)
-    active_linters = State['active_linters_per_bid'][buffer_id]
-    for window in sublime.windows():
-        for view in window.views():
-            if view.buffer_id() == buffer_id:
-                draw(view, active_linters)
+    for view in views_by_buffer(buffer_id):
+        if view.buffer_id() == buffer_id:
+            draw(view, problems)
+
+
+def views_by_buffer(bid):
+    return (view for window in sublime.windows() for view in window.views())
+
+
+def count_problems(errors):
+    w, e = 0, 0
+    for error in errors:
+        error_type = error['error_type']
+        if error_type == WARNING:
+            w += 1
+        elif error_type == ERROR:
+            e += 1
+    return (w, e)
 
 
 class sublime_linter_assigned(sublime_plugin.WindowCommand):
     def run(self, bid, linter_names):
-        current_state = State['active_linters_per_bid'][bid]
+        current_state = State['assigned_linters_per_bid'][bid]
         next_state = set(linter_names)
         if current_state == next_state:
             return
 
-        State['active_linters_per_bid'][bid] = next_state
-        State['needs_redraw'].add(bid)
+        State['assigned_linters_per_bid'][bid] = next_state
+        State['failed_linters_per_bid'][bid] = set()
 
 
 class sublime_linter_deactivated(sublime_plugin.WindowCommand):
     def run(self, bid, linter_name):
-        set_active_status(bid, linter_name, active=False)
+        State['failed_linters_per_bid'][bid].add(linter_name)
 
 
-def set_active_status(bid, linter_name, active):
-    active_linters = State['active_linters_per_bid'][bid]
-    current_state = linter_name in active_linters
-    if current_state == active:
-        return
-
-    if active:
-        active_linters.add(linter_name)
-    else:
-        active_linters.discard(linter_name)
-
-    State['needs_redraw'].add(bid)
+class UpdateState(sublime_plugin.EventListener):
+    # Fires once per view with the actual view, not necessary the primary
+    def on_activated_async(self, active_view):
+        draw(active_view, State['problems_per_bid'][active_view.buffer_id()])
 
 
-def draw(view, active_linters):
+def draw(view, problems):
     if persist.settings.get('statusbar.show_active_linters'):
-        view.set_status(STATUS_ACTIVE_KEY, ', '.join(sorted(active_linters)))
+        message = ', '.join(
+            '{}{}'.format(linter_name, summary)
+            for linter_name, summary in sorted(problems.items())
+        )
+        view.set_status(STATUS_ACTIVE_KEY, message)
     else:
         view.erase_status(STATUS_ACTIVE_KEY)

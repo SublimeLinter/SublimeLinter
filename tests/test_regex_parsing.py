6781ee10a4c9a34bdae2e697bc2097c06cf6796c
herr kaste <herr.kaste@gmail.com>
2018-08-30 13:56:59 +0200

    Add `reposition_match` tests

--- tests/test_regex_parsing.py
+++ tests/test_regex_parsing.py
@@ -1,10 +1,11 @@
 from functools import partial
+import unittest
 
 import sublime
-from SublimeLinter.lint import Linter, backend
+from SublimeLinter.lint import Linter, backend, persist, linter as linter_module
 from unittesting import DeferrableTestCase
 
-from SublimeLinter.tests.mockito import when, patch, unstub
+from SublimeLinter.tests.mockito import when, patch, unstub, spy2 as spy
 
 version = sublime.version()
 
@@ -17,9 +18,8 @@ execute_lint_task = partial(
 class FakeLinter1(Linter):
     defaults = {'selector': 'NONE'}
     cmd = 'fake_linter_1'
-    regex = (
-        r'^stdin:(?P<line>\d+):(?P<col>\d+)\s(?P<error>ERROR):\s(?P<message>.*)$'
-    )
+    regex = r"^stdin:(?P<line>\d+):(?P<col>\d+)?\s(?P<error>ERROR):\s(?P<near>'[^']+')?(?P<message>.*)$"
+    line_col_base = (1, 1)
 
 
 class TestRegexBasedParsing(DeferrableTestCase):
@@ -125,7 +125,7 @@ class TestRegexBasedParsing(DeferrableTestCase):
             result,
         )
 
-    def test_minimum_length_of_region_is_one(self):
+    def test_if_col_and_not_on_a_word_set_length_1(self):
         linter = self.create_linter()
 
         INPUT = "    This is the source code."  # <===========
@@ -136,14 +136,157 @@ class TestRegexBasedParsing(DeferrableTestCase):
         drop_info_keys(result)
 
         self.assertResult(
+            [{'line': 0, 'start': 0, 'end': 1, 'region': sublime.Region(0, 1)}],
+            result,
+        )
+
+    @unittest.expectedFailure
+    def test_if_no_col_and_no_near_mark_line(self):
+        # FIXME: Reported end is currently 9, but must be 10
+
+        spy(persist.settings.get)
+        when(persist.settings).get('no_column_highlights_line').thenReturn(True)
+
+        linter = self.create_linter()
+
+        INPUT = "0123456789"
+        OUTPUT = "stdin:1: ERROR: The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
             [
                 {
                     'line': 0,
                     'start': 0,
-                    'end': 1,  # <============
-                    'region': sublime.Region(0, 1),
+                    'end': 10,
+                    'region': sublime.Region(0, 10),
                 }
             ],
+            result,
+        )
+
+    def test_mark_line_should_not_select_the_trailing_newline_char(self):
+        # NOTE: Bc underlining the trailing \n looks ugly
+
+        spy(persist.settings.get)
+        when(persist.settings).get('no_column_highlights_line').thenReturn(True)
+
+        linter = self.create_linter()
+
+        INPUT = "0123456789\n"
+        OUTPUT = "stdin:1: ERROR: The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [
+                {
+                    'line': 0,
+                    'start': 0,
+                    'end': 10,
+                    'region': sublime.Region(0, 10),
+                }
+            ],
+            result,
+        )
+
+    def test_if_no_col_and_no_near_mark_zero(self):
+        # NOTE: 'end' == 0 but Region still (0, 1)
+        # Proposed: Region(0, 10), but start and end == None
+        # Wanted sideeffect: no underlining but just a gutter mark
+
+        spy(persist.settings.get)
+        when(persist.settings).get('no_column_highlights_line').thenReturn(False)
+
+        linter = self.create_linter()
+
+        INPUT = "0123456789"
+        OUTPUT = "stdin:1: ERROR: The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [{'line': 0, 'start': 0, 'end': 0, 'region': sublime.Region(0, 1)}],
+            result,
+        )
+
+    def test_if_col_and_near_set_length(self):
+        linter = self.create_linter()
+
+        INPUT = "0123456789"
+        OUTPUT = "stdin:1:2 ERROR: '....' The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [{'line': 0, 'start': 1, 'end': 5, 'region': sublime.Region(1, 5)}],
+            result,
+        )
+
+    def test_if_no_col_but_near_search_term(self):
+        linter = self.create_linter()
+
+        INPUT = "0123 foo 456789"
+        OUTPUT = "stdin:1: ERROR: 'foo' The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [{'line': 0, 'start': 5, 'end': 8, 'region': sublime.Region(5, 8)}],
+            result,
+        )
+
+    def test_if_no_col_but_near_and_search_fails_select_line(self):
+        spy(persist.settings.get)
+        when(persist.settings).get('no_column_highlights_line').thenReturn(True)
+
+        linter = self.create_linter()
+
+        INPUT = "0123456789\n"
+        OUTPUT = "stdin:1: ERROR: 'foo' The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [
+                {
+                    'line': 0,
+                    'start': 0,
+                    'end': 10,
+                    'region': sublime.Region(0, 10),
+                }
+            ],
+            result,
+        )
+
+    def test_if_no_col_but_near_and_search_fails_select_zero(self):
+        spy(persist.settings.get)
+        when(persist.settings).get('no_column_highlights_line').thenReturn(False)
+
+        linter = self.create_linter()
+
+        INPUT = "0123456789\n"
+        OUTPUT = "stdin:1: ERROR: 'foo' The message"
+        when(linter)._communicate(['fake_linter_1'], INPUT).thenReturn(OUTPUT)
+
+        result = execute_lint_task(linter, INPUT)
+        drop_info_keys(result)
+
+        self.assertResult(
+            [{'line': 0, 'start': 0, 'end': 0, 'region': sublime.Region(0, 1)}],
             result,
         )
 

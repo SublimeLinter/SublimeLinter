caa52cea4c31fc45f8538db7bb6b5726dbf8a1d5
ew4321 <andrewykim99@gmail.com>
2018-10-07 18:56:43 -0400

    made node_linter.py pass flake8

--- lint/base_linter/node_linter.py
+++ lint/base_linter/node_linter.py
@@ -58,8 +58,10 @@ class NodeLinter(linter.Linter):
             return True, global_cmd
         else:
             logger.warning('{} cannot locate \'{}\'\n'
-                           'Please refer to the readme of this plugin and our troubleshooting guide: '
-                           'http://www.sublimelinter.com/en/stable/troubleshooting.html'.format(self.name, cmd[0]))
+                           'Please refer to the readme of this plugin '
+                           'and our troubleshooting guide: '
+                           'http://www.sublimelinter.com/en/stable/'
+                           'troubleshooting.html'.format(self.name, cmd[0]))
             return True, None
 
     def get_manifest_path(self):
@@ -109,8 +111,9 @@ class NodeLinter(linter.Linter):
         return self.find_ancestor_cmd_path(cmd, cwd)
 
     def find_ancestor_cmd_path(self, cmd, cwd):
-        """Recursively check for command binary in ancestors' node_modules/.bin directories."""
-        node_modules_bin = os.path.normpath(os.path.join(cwd, 'node_modules/.bin/'))
+        """Recursively finds command binary in node_modules/.bin folders."""
+        node_modules_bin = os.path.normpath(os.path.join(
+            cwd, 'node_modules/.bin/'))
 
         binary = shutil.which(cmd, path=node_modules_bin)
         if binary:
@@ -152,7 +155,8 @@ class NodeLinter(linter.Linter):
         """Read manifest and cache mtime, hash and json content."""
         self.cached_manifest_mtime = current_manifest_mtime
         self.cached_manifest_hash = self.hash_manifest()
-        self.cached_manifest = json.load(codecs.open(self.manifest_path, 'r', 'utf-8'))
+        self.cached_manifest = json.load(
+            codecs.open(self.manifest_path, 'r', 'utf-8'))
 
     def hash_manifest(self):
         """Calculate the hash of the manifest file."""

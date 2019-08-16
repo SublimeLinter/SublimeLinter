4241bb9f723dfe664e1f07fa2829987f071b5f06
herr kaste <herr.kaste@gmail.com>
2019-08-16 17:51:45 +0200

    Stash `util.print_runtime` to measure performance

--- lint/util.py
+++ lint/util.py
@@ -1,5 +1,5 @@
 """This module provides general utility methods."""
-
+from contextlib import contextmanager
 from functools import lru_cache, wraps
 import locale
 import logging
@@ -8,6 +8,8 @@ import os
 import re
 import sublime
 import subprocess
+import time
+import threading
 
 
 if False:
@@ -30,6 +32,16 @@ def printf(*args):
     for arg in args:
         print(arg, end=' ')
     print()
+
+
+@contextmanager
+def print_runtime(message):
+    start_time = time.perf_counter()
+    yield
+    end_time = time.perf_counter()
+    duration = round((end_time - start_time) * 1000)
+    thread_name = threading.current_thread().name[0]
+    print('{} took {}ms [{}]'.format(message, duration, thread_name))
 
 
 def show_message(message, window=None):

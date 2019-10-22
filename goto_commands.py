SublimeLinter/SublimeLinter

commit 333ebc2c6c6842b0fe245ee4da70e74a5b1a1fe4

Avoid scroll if region is already visible on goto error
If the region we're moving to is already visible, then we don't want
the view to suddenly scroll. If the region is not visible, then we want
the surrounding area of the region to be visible.

goto_commands.py
@@ -83,11 +83,31 @@ def goto(view, direction, count, wrap):
     move_to(view, line, start)
 
 
+class _sublime_linter_goto_line(sublime_plugin.TextCommand):
+
+    def run(self, edit, line, col):
+        pt = self.view.text_point(line, col)
+        self.view.sel().clear()
+        self.view.sel().add(pt)
+        self.view.show(pt)
+
+
 def move_to(view, line, col):
     window = view.window()
-    filename = view.file_name() or "<untitled {}>".format(view.buffer_id())
-    target = "{}:{}:{}".format(filename, line + 1, col + 1)
-    window.open_file(target, sublime.ENCODED_POSITION)
+    if view == window.active_view():
+        # If the region we're moving to is already visible, then we don't want
+        # the view to suddenly scroll. If the region is not visible, then we
+        # want the surrounding area of the region to be visible.
+        # We need to a use a custom goto line command for several reasons:
+        # * ST's goto line command doesn't accept a col argument.
+        # * SL requires that on_selection_modified events MUST be triggered for
+        #   each move.
+        # See https://github.com/SublimeLinter/SublimeLinter/pull/867.
+        view.run_command('_sublime_linter_goto_line', {'line': line, 'col': col})
+    else:
+        filename = view.file_name() or "<untitled {}>".format(view.buffer_id())
+        target = "{}:{}:{}".format(filename, line + 1, col + 1)
+        window.open_file(target, sublime.ENCODED_POSITION)
 
 
 def get_current_pos(view):

commit d63ebc35becb5e47935150d7932af6e6f7a1550a

Fix: Goto should participate in Sublime's move-history
Actually, a comment here suggest that this works but Sublime's
inbuilt `go back/forth` functionality was broken since 333ebc2.

We participate with the `history_list` by manipulating it
imperativly.

Notes:

- We convert the command to a `TextCommand` bc that  is a better fit.

- After that, we *technically* don't need the
  `_sublime_linter_move_cursor` command (bc we're already in a
  `TextCommand`) but since `move_to` is a function we could call from
  everywhere we keep the invariant local for now. :shrug:

goto_commands.py
@@ -1,24 +1,28 @@
 import sublime
 import sublime_plugin
+from Default import history_list
 
 from itertools import dropwhile, takewhile
 
 from .lint import persist, util
 
 
-"""
-Implement typical Goto Next Previous Error Commands.
-"""
+MYPY = False
+if MYPY:
+    from typing import Union
+    from typing_extensions import Literal
+
+    Direction = Union[Literal['next'], Literal['previous']]
 
 
-class SublimeLinterGotoError(sublime_plugin.WindowCommand):
-    def run(self, direction='next', count=1, wrap=False):
-        active_view = self.window.active_view()
-        if active_view:
-            goto(active_view, direction, count, wrap)
+class SublimeLinterGotoError(sublime_plugin.TextCommand):
+    def run(self, edit, direction='next', count=1, wrap=False):
+        # type: (sublime.Edit, Direction, int, bool) -> None
+        goto(self.view, direction, count, wrap)
 
 
 def goto(view, direction, count, wrap):
+    # type: (sublime.View, Direction, int, bool) -> None
     filename = util.get_filename(view)
     errors = persist.file_errors.get(filename)
     if not errors:
@@ -74,8 +78,9 @@ def goto(view, direction, count, wrap):
 
 
 class _sublime_linter_move_cursor(sublime_plugin.TextCommand):
-    # We want to ensure that `on_selection_modified` handlers run and the
-    # 'move-back' (editing history) still works.
+    # We ensure `on_selection_modified` handlers run by using a `TextCommand`.
+    # See: https://github.com/SublimeLinter/SublimeLinter/pull/867
+    # and https://github.com/SublimeTextIssues/Core/issues/485#issuecomment-337480388
     def run(self, edit, point):
         self.view.sel().clear()
         self.view.sel().add(point)
@@ -83,16 +88,12 @@ class _sublime_linter_move_cursor(sublime_plugin.TextCommand):
 
 
 def move_to(view, point):
-    window = view.window()
-    if view == window.active_view():
-        view.run_command('_sublime_linter_move_cursor', {'point': point})
-    else:
-        filename = util.get_filename(view)
-        line, col = view.rowcol(point)
-        target = "{}:{}:{}".format(filename, line + 1, col + 1)
-        window.open_file(target, sublime.ENCODED_POSITION)
+    # type: (sublime.View, int) -> None
+    history_list.get_jump_history_for_view(view).push_selection(view)
+    view.run_command('_sublime_linter_move_cursor', {'point': point})
 
 
 def flash(view, msg):
+    # type: (sublime.View, str) -> None
     window = view.window() or sublime.active_window()
     window.status_message(msg)

mypy.ini
@@ -5,6 +5,9 @@ warn_unused_ignores = True
 mypy_path = stubs
 sqlite_cache = True
 
+[mypy-Default]
+ignore_missing_imports = True
+
 [mypy-unittesting]
 ignore_missing_imports = True
 

stubs/sublime.pyi
@@ -232,7 +232,7 @@ class Selection(Sized):
     def __bool__(self) -> bool: ...
     def is_valid(self) -> bool: ...
     def clear(self) -> None: ...
-    def add(self, x: Region) -> None: ...
+    def add(self, x: Union[Region, Point]) -> None: ...
     def add_all(self, regions: Sequence[Region]) -> None: ...
     def subtract(self, region: Region) -> None: ...
     def contains(self, region: Region) -> bool: ...
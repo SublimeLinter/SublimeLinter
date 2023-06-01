9bce37c5648490f61f55939091e816b76faa74a5
herr kaste <herr.kaste@gmail.com>
2023-06-01 21:51:41 +0200

    Move panel name to `const`

--- lint/const.py
+++ lint/const.py
@@ -3,3 +3,6 @@ IS_ENABLED_SWITCH = "SublimeLinter.enabled?"
 
 WARNING = "warning"
 ERROR = "error"
+
+ERROR_PANEL_NAME = "SublimeLinter Messages"
+ERROR_OUTPUT_PANEL = "output." + ERROR_PANEL_NAME
--- message_view.py
+++ message_view.py
@@ -1,13 +1,12 @@
 import sublime
 import sublime_plugin
 
-PANEL_NAME = "SublimeLinter Messages"
-OUTPUT_PANEL = "output." + PANEL_NAME
+from .lint.const import ERROR_PANEL_NAME, ERROR_OUTPUT_PANEL
 
 
 def plugin_unloaded():
     for window in sublime.windows():
-        window.destroy_output_panel(PANEL_NAME)
+        window.destroy_output_panel(ERROR_PANEL_NAME)
 
 
 class sublime_linter_display_panel(sublime_plugin.WindowCommand):
@@ -15,10 +14,10 @@ class sublime_linter_display_panel(sublime_plugin.WindowCommand):
         window = self.window
 
         if is_panel_active(window):
-            panel = window.find_output_panel(PANEL_NAME)
+            panel = window.find_output_panel(ERROR_PANEL_NAME)
             assert panel
         else:
-            panel = window.create_output_panel(PANEL_NAME)
+            panel = window.create_output_panel(ERROR_PANEL_NAME)
             syntax_path = "Packages/SublimeLinter/panel/message_view.sublime-syntax"
             try:  # Try the resource first, in case we're in the middle of an upgrade
                 sublime.load_resource(syntax_path)
@@ -34,13 +33,13 @@ class sublime_linter_display_panel(sublime_plugin.WindowCommand):
         panel.run_command('append', {'characters': msg})
         panel.set_read_only(True)
         panel.show(scroll_to)
-        window.run_command("show_panel", {"panel": OUTPUT_PANEL})
+        window.run_command("show_panel", {"panel": ERROR_OUTPUT_PANEL})
 
 
 class sublime_linter_remove_panel(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.destroy_output_panel(PANEL_NAME)
+        self.window.destroy_output_panel(ERROR_PANEL_NAME)
 
 
 def is_panel_active(window):
-    return window.active_panel() == OUTPUT_PANEL
+    return window.active_panel() == ERROR_OUTPUT_PANEL

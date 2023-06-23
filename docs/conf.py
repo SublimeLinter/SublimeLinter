a9632c17519091bba734b2e78b7e8a6fb7d8bebb
herr kaste <herr.kaste@gmail.com>
2023-06-23 19:32:15 +0200

    Switch to the default alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@ All configuration values have a default; values that are commented out
 serve to show the default.
 """
 
-import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -98,14 +97,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

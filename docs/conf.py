d2cff76a90cf74f67a520d334fb997a6868459cf
herr kaste <herr.kaste@gmail.com>
2023-06-24 02:33:03 +0200

    Update copyright year in the footer

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Sublime\nLinter'
-copyright = '2018, The SublimeLinter Community'
+copyright = '2018-2023, The SublimeLinter Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
